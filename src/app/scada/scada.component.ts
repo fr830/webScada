186dfc4c5d5edc9652f28d3cc656eab97a7218d6
MoverLee <leejaymover@gmail.com>
2017-11-22 21:56:04 +0800

    Function Or Bug: N/A; Symption: N/A; BugId: N/A; Root Cause: N/A; Solution: N/A; Note: N/A;

--- src/app/scada/scada.component.ts
+++ src/app/scada/scada.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-import { $ } from 'jquery';
 import { raphael } from 'raphael';
 import { morris } from 'morris';
 @Component({
@@ -10,11 +9,50 @@ import { morris } from 'morris';
   encapsulation: ViewEncapsulation.None
 })
 export class ScadaComponent implements OnInit {
+  private tempalteArray = [
+    'horizontal-pipe',
+     'vertical-pipe',
+     'top-valve',
+     'bottom-valve',
+     'right-valve',
+     'left-valve',
+     'crosspipe',
+     'top-tway',
+     'bottom-tway',
+     'right-tway',
+     'left-tway',
+     'left-top-elbow',
+     'right-top-elbow',
+     'left-bottom-elbow',
+     'right-bottom-elbow',
+     'vertical-tank',
+     'horizontal-tank',
+     'cutaway',
+     'pump',
+     'pipe-end-bottom',
+     'pipe-end-top',
+     'pipe-end-right',
+     'pipe-end-left',
+     'vertical-rotor',
+     'horizontal-rotor'
+  ];
 
   constructor() {
   }
 
   ngOnInit() {
+
+      for ( let i of this.tempalteArray) {
+          const tagContent = this.getTagInnerHTML( 'app-' + i);
+          this.appendChild( 'svg-show-content', tagContent);
+      }
   }
 
+  private getTagInnerHTML( tagName) {
+      return document.getElementsByTagName('' + tagName + '')[0].innerHTML;
+  }
+
+  private appendChild( ele, child) {
+      document.getElementsByClassName('' + ele + '')[0].innerHTML += child;
+  }
 }
