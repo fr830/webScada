06d9fcccab6a75c3fe8abe4cf7fa5a5b9216bd93
MoverLee <leejaymover@gmail.com>
2017-11-24 18:17:09 +0800

    Function Or Bug: N/A; Symption: N/A; BugId: N/A; Root Cause: N/A; Solution: N/A; Note: N/A;

--- src/app/scada/bottom-tway/bottom-tway.component.ts
+++ src/app/scada/bottom-tway/bottom-tway.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-bottom-tway',
   templateUrl: './bottom-tway.component.html',
-  styleUrls: ['./bottom-tway.component.css'],
+  styleUrls: ['./bottom-tway.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class BottomTwayComponent implements OnInit {
